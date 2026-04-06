259423658af632cec3aa345e14540dfe0ecd25e8
Юлиана <nastya.pup93@mail.ru>
2026-04-06 20:57:42 +0300

    задание4

--- Derevyannyh_JK/task4/source.cpp
+++ Derevyannyh_JK/task4/source.cpp
@@ -6,157 +6,70 @@ using namespace std;
 
 struct Date {
     int d, m, y;
-    Date(int d = 1, int m = 1, int y = 2026) : d(d), m(m), y(y) {}
-    bool operator<(const Date& other) const {
-        if (y != other.y) return y < other.y;
-        if (m != other.m) return m < other.m;
-        return d < other.d;
+    Date(int d = 1, int m = 1, int y = 2026) :d(d), m(m), y(y) {}
+    bool operator<(const Date& o)const {
+        if (y != o.y)return y < o.y;
+        if (m != o.m)return m < o.m;
+        return d < o.d;
     }
 };
 
-struct Time {
-    int h;
-    Time(int h = 0) : h(h) {}
-    bool operator<(const Time& other) const { return h < other.h; }
-};
+struct Time { int h; Time(int h = 0) :h(h) {} bool operator<(const Time& o)const { return h < o.h; } };
 
 class Thermometer {
-private:
-    map<Date, map<Time, double>> data;
-    Date startDate;
-    Time startTime;
-
+    map<Date, map<Time, double>> d;
+    Date sd; Time st;
 public:
-    Thermometer() : startDate(1, 1, 2026), startTime(0) {}
-
-    void setStart(Date d, Time t) { startDate = d; startTime = t; }
-
-    void getStart(Date& d, Time& t) { d = startDate; t = startTime; }
-
-    void setObs(Date d, Time t, double temp) { data[d][t] = temp; }
-
-    double getTemp(Date d, Time t) {
-        if (data.count(d) && data[d].count(t)) return data[d][t];
-        return -999;
+    Thermometer() :sd(1, 1, 2026), st(0) {}
+    void setStart(Date a, Time b) { sd = a; st = b; }
+    void getStart(Date& a, Time& b) { a = sd; b = st; }
+    void setObs(Date a, Time b, double t) { d[a][b] = t; }
+    double getTemp(Date a, Time b) { return d.count(a) && d[a].count(b) ? d[a][b] : -999; }
+    void setSeries(Date a, vector<double> v) { for (int i = 0; i < 24; i++)d[a][Time(i)] = v[i]; }
+    double avgDate(Date a) {
+        double s = 0; int c = 0;
+        for (auto& p : d[a])s += p.second, c++;
+        return c ? s / c : 0;
     }
-
-    void setSeries(Date d, vector<double> temps) {
-        for (int i = 0; i < 24; i++) data[d][Time(i)] = temps[i];
-    }
-
-    double avgDate(Date d) {
-        double sum = 0; int cnt = 0;
-        for (auto& p : data[d]) { sum += p.second; cnt++; }
-        return cnt ? sum / cnt : 0;
-    }
-
     double avgMonth(int m) {
-        double sum = 0; int cnt = 0;
-        for (auto& dateObs : data)
-            if (dateObs.first.m == m)
-                for (auto& timeObs : dateObs.second)
-                {
-                    sum += timeObs.second; cnt++;
-                }
-        return cnt ? sum / cnt : 0;
+        double s = 0; int c = 0;
+        for (auto& a : d)if (a.first.m == m)for (auto& b : a.second)s += b.second, c++;
+        return c ? s / c : 0;
     }
-
     double avgAll() {
-        double sum = 0; int cnt = 0;
-        for (auto& dateObs : data)
-            for (auto& timeObs : dateObs.second)
-            {
-                sum += timeObs.second; cnt++;
+        double s = 0; int c = 0;
+        for (auto& a : d)for (auto& b : a.second)s += b.second, c++;
+        return c ? s / c : 0;
+    }
+    double avgDayNight(int m, bool day) {
+        double s = 0; int c = 0;
+        for (auto& a : d)if (a.first.m == m)
+            for (auto& b : a.second) {
+                int h = b.first.h;
+                if ((day && h >= 6 && h <= 18) || (!day && (h >= 19 || h <= 5)))s += b.second, c++;
             }
-        return cnt ? sum / cnt : 0;
+        return c ? s / c : 0;
     }
-
-    double avgDayNight(int m, bool isDay) {
-        double sum = 0; int cnt = 0;
-        for (auto& dateObs : data)
-            if (dateObs.first.m == m)
-                for (auto& timeObs : dateObs.second) {
-                    int h = timeObs.first.h;
-                    if ((isDay && h >= 6 && h <= 18) || (!isDay && (h >= 19 || h <= 5)))
-                    {
-                        sum += timeObs.second; cnt++;
-                    }
-                }
-        return cnt ? sum / cnt : 0;
+    void save(string f) {
+        ofstream o(f);
+        o << sd.d << " " << sd.m << " " << sd.y << " " << st.h << endl;
+        for (auto& a : d)for (auto& b : a.second)
+            o << a.first.d << " " << a.first.m << " " << a.first.y << " " << b.first.h << " " << b.second << endl;
     }
-
-    void save(string fname) {
-        ofstream f(fname);
-        f << startDate.d << " " << startDate.m << " " << startDate.y << " " << startTime.h << endl;
-        for (auto& dateObs : data)
-            for (auto& timeObs : dateObs.second)
-                f << dateObs.first.d << " " << dateObs.first.m << " " << dateObs.first.y << " "
-                << timeObs.first.h << " " << timeObs.second << endl;
-    }
-
-    void load(string fname) {
-        ifstream f(fname);
-        data.clear();
-        f >> startDate.d >> startDate.m >> startDate.y >> startTime.h;
-        int d, m, y, h; double temp;
-        while (f >> d >> m >> y >> h >> temp)
-            data[Date(d, m, y)][Time(h)] = temp;
+    void load(string f) {
+        ifstream i(f);
+        d.clear();
+        i >> sd.d >> sd.m >> sd.y >> st.h;
+        int dd, mm, yy, hh; double t;
+        while (i >> dd >> mm >> yy >> hh >> t)d[Date(dd, mm, yy)][Time(hh)] = t;
     }
 };
 
-// Пример использования
 int main() {
     Thermometer t;
-
-    cout << "=== Термометр. Наблюдения за 2026 год ===\n\n";
-
-    // Задаём наблюдения
     t.setObs(Date(15, 6, 2026), Time(12), 26.5);
-    t.setObs(Date(15, 6, 2026), Time(18), 23.0);
-    t.setObs(Date(20, 12, 2026), Time(8), -5.5);
-    t.setObs(Date(20, 12, 2026), Time(14), -2.0);
-
-    // Устанавливаем начальную дату
-    t.setStart(Date(1, 1, 2026), Time(0));
-
-    // Получаем начальную дату
-    Date sd; Time st;
-    t.getStart(sd, st);
-    cout << "Начало наблюдений: " << sd.d << "." << sd.m << "." << sd.y << " " << st.h << ":00\n\n";
-
-    // Выводим температуры
-    cout << "Температура 15.06.2026 12:00: " << t.getTemp(Date(15, 6, 2026), Time(12)) << "°C\n";
-    cout << "Температура 15.06.2026 18:00: " << t.getTemp(Date(15, 6, 2026), Time(18)) << "°C\n";
-    cout << "Температура 20.12.2026 08:00: " << t.getTemp(Date(20, 12, 2026), Time(8)) << "°C\n\n";
-
-    // Средние температуры
-    cout << "Средняя за 15 июня: " << t.avgDate(Date(15, 6, 2026)) << "°C\n";
-    cout << "Средняя за июнь: " << t.avgMonth(6) << "°C\n";
-    cout << "Средняя за декабрь: " << t.avgMonth(12) << "°C\n";
-    cout << "Средняя за весь год: " << t.avgAll() << "°C\n\n";
-
-    // Дневная/ночная температура
-    cout << "Дневная средняя за июнь: " << t.avgDayNight(6, true) << "°C\n";
-    cout << "Ночная средняя за июнь: " << t.avgDayNight(6, false) << "°C\n";
-    cout << "Дневная средняя за декабрь: " << t.avgDayNight(12, true) << "°C\n";
-    cout << "Ночная средняя за декабрь: " << t.avgDayNight(12, false) << "°C\n\n";
-
-    // Серия наблюдений для одного дня
-    cout << "Записываем серию наблюдений для 1 января 2026...\n";
-    vector<double> jan1temps(24);
-    for (int i = 0; i < 24; i++) jan1temps[i] = -10 + i * 0.5;
-    t.setSeries(Date(1, 1, 2026), jan1temps);
-
-    cout << "Средняя за 1 января: " << t.avgDate(Date(1, 1, 2026)) << "°C\n\n";
-
-    // Сохраняем и загружаем
-    t.save("weather_2026.txt");
-    cout << "Данные сохранены в weather_2026.txt\n";
-
-    Thermometer t2;
-    t2.load("weather_2026.txt");
-    cout << "Данные загружены из файла\n";
-    cout << "Проверка: температура 15.06.2026 12:00 = " << t2.getTemp(Date(15, 6, 2026), Time(12)) << "°C\n";
-
-    return 0;
+    t.setObs(Date(15, 6, 2026), Time(18), 23);
+    cout << t.getTemp(Date(15, 6, 2026), Time(12)) << endl;
+    cout << t.avgMonth(6) << endl;
+    t.save("w.txt");
 }
